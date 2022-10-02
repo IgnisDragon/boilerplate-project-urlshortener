a7cffac294b16e63ab7bdb1f92fe358cbe6e481c
IgnisDragon <qwas565l@yahoo.com.tw>
2022-10-03 01:32:10 +0800

    completed

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use(cors());
 
@@ -14,7 +18,45 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+var urls = {};
+var count = 1;
 // Your first API endpoint
+app.get('/api/shorturl/:short_url', function(req, res) {
+
+  let short_url = req.params.short_url;
+  console.log(short_url)
+
+  if (short_url in urls) {
+
+    res.redirect(urls[short_url]);
+  }
+  else {
+
+    res.json({error: 'invalid url'});
+  }
+});
+
+app.post("/api/shorturl", function(req, res) {
+
+  const URL_REGEX = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/
+
+  let original = req.body.url;
+
+  if (!URL_REGEX.test(original)) {
+      
+    res.json({error: 'invalid url'});
+  }
+  else {
+  
+    let short = count;
+    urls[count++] = original;
+    console.log(original);
+
+    res.json({original_url : original,
+              short_url : short});
+  }
+})
+
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
